b9bb223ec4c91af0af13a1a9f4bd2e3f3b22502a
Rindadin <mohamadmohidin@gmail.com>
2018-08-09 13:44:31 +0800

    edit in segment

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
 import { NavController } from 'ionic-angular';
+import{ Chart } from 'chart.js';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  @ViewChild('pieCanvas') pieCanvas;
+  pieChart: any;
+  
+constructor (){}
+  IonViewDidLoad(){
+   
+      this.pieChart = new Chart(this.pieCanvas.nativeElement,{
+        type: 'pie',
+        data: {
+          labels: ['kerosakkan','Bencana','Ganguan'],
+          datasets: [{
+            backgroundcolor: ['blue','red','white'],
+            data:[4,5,6],
+            hoverBackgroundcolor: ['blue','red','white']
+          }]
+        }
+      });
+    }
+    
   }
 
-}
