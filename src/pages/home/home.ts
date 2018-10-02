49cbb7dad1c204a83422789bc1d0c72684fb1e92
Rindadin <mohamadmohidin@gmail.com>
2018-10-02 13:00:58 +0800

    profile

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DashboardPage } from '../dashboard/dashboard';
+import { RegisterPage } from '../register/register';
+import { ProfilePage } from '../profile/profile';
+import { PendingPage } from '../pending/pending';
+import { AssetPage } from '../asset/asset';
 
 /**
  * Generated class for the HomePage page.
@@ -20,6 +25,26 @@ export class HomePage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
+  }
+
+  goToDashboard(){
+    this.navCtrl.setRoot(DashboardPage, {}, {animate: true});
+  }
+
+  goToRegister(){
+    this.navCtrl.setRoot(RegisterPage, {}, {animate: true});
+  }
+
+  goToProfile(){
+    this.navCtrl.setRoot(ProfilePage, {}, {animate: true});
+  }
+
+  goToPending(){
+    this.navCtrl.setRoot(PendingPage, {}, {animate: true});
+  }
+
+  goToAsset(){
+    this.navCtrl.setRoot(AssetPage, {}, {animate: true});
   }
 
 }
